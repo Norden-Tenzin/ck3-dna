3ab314fc32be841150cc839d2e64009112b1e093
Tenzin Norden <nordentenzin29@gmail.com>
2023-04-19 17:06:13 +0530

    login and navigation

---
+++ src/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './routes/Login';
+import Register from './routes/Register';
+import Reset from './routes/Reset';
+import Dashboard from './routes/Dashboard';
+import LaunchPage from './routes/LaunchPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/reset",
+    element: <Reset />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/launch",
+    element: <LaunchPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
